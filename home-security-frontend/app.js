132b72800e0e71623998e1d25db4c91b40c61811
Logan Han <logan@han.life>
2018-08-10 15:00:57 +1000

    MVP

--- home-security-frontend/app.js
+++ home-security-frontend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dateTime = require('date-time');
+var arraySort = require('array-sort');
 var config = require("./config.js");
 
 app.engine('html', require('ejs').renderFile);
@@ -12,13 +14,43 @@ app.get('/', (req, res) => {
       TableName: config.tableName
     }
     dynamodb.scan(params, function(err, data) {
-      if (err) {
-        console.log(err);
-      }
+      if (err) console.log(err);
       else {
-        console.log(JSON.stringify(data.Items));
-        res.render('listall',{items : data.Items, config : config});
+        for(var i = 0; i < data.Items.length; i++) data.Items[i].date = dateTime({date: new Date(data.Items[i].timestamp)});
+        res.render('list',{items : arraySort(data.Items, 'timestamp', {reverse: true}), config : config});
       }
+    });
+});
+
+app.get('/by-filename/:filename', (req, res) => {
+    var params = {
+      TableName: config.tableName,
+      FilterExpression: "filename = :filename",
+      ExpressionAttributeValues: {
+        ":filename": req.params.filename
+      }
+    }
+    dynamodb.scan(params, function(err, data) {
+      if (err) console.log(err);
+      else {
+        res.render('detail',{items : data.Items, date : dateTime({date: new Date(data.Items[0].timestamp)}), config : config});
+      }
+    });
+});
+
+  app.get('/by-faceId/:faceId', (req, res) => {
+      var params = {
+        TableName: config.tableName,
+        FilterExpression: "faceId = :faceId",
+        ExpressionAttributeValues: {
+          ":faceId": req.params.faceId
+        }
+      }
+      dynamodb.scan(params, function(err, data) {
+        if (err) console.log(err);
+        else {
+          res.render('detail',{items : arraySort(data.Items, 'timestamp', {reverse: true}), date : dateTime({date: new Date(data.Items[0].timestamp)}), config : config});
+        }
     });
 });
 

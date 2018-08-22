00a3ba259b6937cae4c240def40b3d72a0eb62c1
Logan Han <logan@han.life>
2018-08-22 11:56:19 +1000

    Add email notification feature

--- home-security-backend/config.js
+++ home-security-backend/config.js
@@ -1,12 +1,17 @@
 var config = {
   tableName: "home_security",
   collectionId: "home-securtiy-cam-face",
-  telstra_clientId: "meow",
-  telstra_Secret: "meow",
-  sms_to_phone: "+61meow",
-  sms_prefix: "Human movement detected, see: ",
-  image_prefix: "https://s3-ap-southeast-2.amazonaws.com/meow/",
+  telstra_clientId: "blah",
+  telstra_Secret: "blah",
+  sms_to_phone: "+61blah",
+  sesRegion: "us-west-2",
+  sesEndpoint: "https://email.us-west-2.amazonaws.com",
+  email_to_address: "blah",
+  email_from_address: "blah",
+  image_prefix: "https://s3-ap-southeast-2.amazonaws.com/bucket/",
+  notification_prefix: "Movement detected ",
   send_sms: "true",
+  send_email: "true",
   debug: "true"
 };
 
